69419f549d0a3300462628c1824e21006ae4dd90
jennysol <jennyssoliver@gmail.com>
2023-05-01 12:05:15 -0300

    update: add image next in app

--- shopping/src/pages/_app.tsx
+++ shopping/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { globalStyles } from '@/styles/global'
 import logoImg from'../assets/Logo.svg'
 import { Container, Header } from '@/styles/pages/app'
+import Image from 'next/image'
 
 globalStyles()
 
@@ -10,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return(
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
 
       <Component {...pageProps} />
