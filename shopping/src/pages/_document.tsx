c2da79d3865a1503a8ae98d612f6faa5c68726f7
jennysol <jennyssoliver@gmail.com>
2023-05-01 11:41:53 -0300

    fix: header location with document

--- shopping/src/pages/_document.tsx
+++ shopping/src/pages/_document.tsx
@@ -10,11 +10,11 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
 
         <style id='stitches' dangerouslySetInnerHTML={{ __html: getCssText() }} />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
-      </Head>
     </Html>
   )
 }
