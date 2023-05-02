a0944a0e61ead6054abdd41d00f617b7b8d6e9e2
jennysol <jennyssoliver@gmail.com>
2023-05-01 21:23:33 -0300

    feature: add format to price products

--- shopping/src/pages/index.tsx
+++ shopping/src/pages/index.tsx
@@ -54,8 +54,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount
-
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount / 100)
     }
   })
 
