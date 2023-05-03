985f94fa6fe46dadfc1b1693da743a52b20c63f6
jennysol <jennyssoliver@gmail.com>
2023-05-02 21:42:27 -0300

    feature: add structure to product page

---
+++ shopping/src/pages/product/[id].tsx
@@ -0,0 +1,20 @@
+import { ProductContainer, ImageContainer, ProductDetails } from "@/styles/pages/product"
+
+export default function Product() {
+  return (
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 20 conto</span>
+        <p>Description tio tiot oitortnro</p>
+
+        <button>Comprar</button>
+      </ProductDetails>
+
+    </ProductContainer>
+  )
+}
